7c626b4ad1023d88af67bbf5e3ab1b6d3f082098
IGOR XAVIER DOS SANTOS <igor.xsantos1@senacsp.edu.br>
2026-03-17 15:23:44 -0300

    aula do dia 17-03

---
+++ testandoShowHide/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testandoShowHide
+{
+    public partial class frmShowHide : Form
+    {
+        public frmShowHide()
+        {
+            InitializeComponent();
+            pbLogotipo.Visible = false;
+        }
+
+        private void ShowHide(object sender, EventArgs e)
+        {
+            if (pbLogotipo.Visible)
+            {
+                pbLogotipo.Hide();
+                btnMostrarLogo.Text = "Mostrar Logo";
+            }
+            else 
+            {
+                pbLogotipo.Show();
+                btnMostrarLogo.Text = "Esconder Logo";
+            }
+        }
+
+        private void pbLogotipo_Click(object sender, EventArgs e)
+        {
+            frmHome form = new frmHome();
+            form.Show();
+            this.Hide();
+        }
+    }
+}
